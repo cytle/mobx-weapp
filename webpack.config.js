623190a4a9f31faa5bd0a2f293c443b5affad680
炒饭 <chaofan@2dfire.com>
2017-05-31 16:06:50 +0800

    mobx+react

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: {
-        'main.js': path.resolve(__dirname, './src/main.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'assets'),
-        filename: '[name]',
-    },
-    plugins: [],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loaders: 'babel-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    performance: {
-        hints: false
-    },
-    devtool: '#inline-source-map'
+  entry: {
+    'main.js': path.resolve(__dirname, './src/main.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: '[name]',
+    publicPath: '/assets/',
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#inline-source-map',
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.vue']
+  }
 }
