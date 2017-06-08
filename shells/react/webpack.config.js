b938f3dd61a2b882513399ed4816ee343fadd66a
炒饭 <chaofan@2dfire.com>
2017-06-08 17:54:19 +0800

    demo todos

--- shells/react/webpack.config.js
+++ shells/react/webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        loaders: [
+          'style-loader?sourceMap',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'less-loader'
+        ]
+      },
+      {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
@@ -53,6 +61,6 @@ module.exports = {
   devtool: '#inline-source-map',
   resolve: {
     alias,
-    extensions: ['.json', '.js', '.jsx']
+    extensions: ['.json', '.js', '.jsx', '.css']
   }
 }
--- src/store/todos/index.js
+++ src/store/todos/index.js
@@ -1,11 +1,36 @@
 import { extendObservable, action } from 'mobx'
-import Todo from './item'
 
-export default function todos () {
+let  uid = 0
+function Todos () {
   extendObservable(this, {
     items: [],
     add: action(function(content) {
-      this.items.push(new Todo(content))
+      this.items.push({
+        startAt: Date.now(),
+        isFinish: false,
+        uid: uid++,
+        content,
+        toggle: action(function() {
+          this.isFinish = !this.isFinish
+        })
+      })
     })
   })
+  this.add('asd')
 }
+
+export default new Todos()
+
+//
+//
+// import { extendObservable, action } from 'mobx'
+// import Todo from './item'
+//
+// export default function todos () {
+//   extendObservable(this, {
+//     items: [],
+//     add: action(function(content) {
+//       this.items.push(new Todo(content))
+//     })
+//   })
+// }
